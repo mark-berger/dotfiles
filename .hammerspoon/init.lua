7cc71ed185efa7ef94fdfde7e64ae41b60cba73e
Mark Berger <maberger6@gmail.com>
2016-12-04 15:50:59 -0500

    Added window resizing to Hammerspoon.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,6 +2,10 @@
 -- @module init
 
 local home = os.getenv("HOME")
+local keys = {"ctrl", "alt", "cmd"}
+
+-- Turn off window animation
+hs.window.animationDuration = 0
 
 --- Reloads Hammerspoon config.
 -- Checks to see if any files end with the .lua extension and then reloads
@@ -19,8 +23,54 @@ function reloadConfig(files)
   end
 end
 
+--- Resizes the active window and moves it to the left or right side.
+-- Cycles between the following sizes: one-half, one-third, one-quarter,
+-- three-quarters, and two-thirds.
+-- @param left Where to position the x coordinate of the window.
+function cycleHorizontal(left)
+  local window = hs.window.focusedWindow()
+  local frame = window:frame()
+  local max = window:screen():frame()
+
+  frame.y = max.y
+  frame.h = max.h
+
+  if frame.w == max.w / 2 then
+    frame.x = left * 2 / 3
+    frame.w = max.w / 3
+  elseif frame.w == math.floor(max.w / 3) then
+    frame.x = left * 3 / 4
+    frame.w = max.w / 4
+  elseif frame.w == max.w / 4 then
+    frame.x = left / 4
+    frame.w = max.w * 3 / 4
+  elseif frame.w == math.floor(max.w * 3 / 4) then
+    frame.x = left / 3
+    frame.w = max.w * 2 / 3
+  else
+    frame.x = left / 2
+    frame.w = max.w / 2
+  end
+
+  window:setFrame(frame)
+end
+
+--- Resizes the active window and moves it to the far left.
+function left()
+  cycleHorizontal(0)
+end
+
+--- Resizes the active window and moves it to the far right.
+function right()
+  cycleHorizontal(hs.window.focusedWindow():screen():frame().w)
+end
+
 -- Watch paths recursively for changes
-hs.pathwatcher.new(home .. "/.hammerspoon/", reloadConfig):start()
+w = hs.pathwatcher.new(home .. "/.hammerspoon/", reloadConfig):start()
+
+-- Setup global keyboard shortcuts
+hs.hotkey.bind(keys, 'Left', left)
+hs.hotkey.bind(keys, 'Right', right)
 
 -- Display a notification when Hammerspoon has finished loading
 hs.notify.show("Hammerspoon reloaded!", "", "~/.hammerspoon/init.lua")

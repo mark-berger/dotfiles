74c0f00461ca34a26fbe4ac62beed92096c4e986
Mark Berger <maberger6@gmail.com>
2018-05-29 20:48:29 -0400

    Simplify t function

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -3,33 +3,6 @@ function t -d "Opens a twitch.tv stream with streamlink"
 
   if test (count $argv) -eq 2
     open "https://www.twitch.tv/$argv[1]/chat?popout="
-
-    switch $argv[2]
-      case a audio
-        set quality audio
-      case mo mobile
-        set quality mobile
-      case l low
-        set quality low
-      case m medium
-        set quality medium
-      case h high
-        set quality high
-      case s source
-        set quality source
-      case 1080p60
-        set quality 1080p60
-      case 1080p
-        set quality 1080p
-      case 900p60
-        set quality 900p60
-      case 720p60
-        set quality 720p60
-      case 720p
-        set quality 720p
-    end
-    streamlink http://www.twitch.tv/$argv[1] $quality
-  else
     streamlink http://www.twitch.tv/$argv[1] $argv[2]
   end
 end

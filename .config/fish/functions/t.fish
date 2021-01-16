fe6bfad7e92d692906e1cbf7c486074ecf98a496
Mark Berger <maberger6@gmail.com>
2021-01-15 23:08:17 -0500

    Update t function

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -2,7 +2,6 @@ function t -d "Opens a twitch.tv stream with streamlink"
   set -l quality
 
   if test (count $argv) -eq 2
-    open "https://www.twitch.tv/$argv[1]/chat?popout="
-    streamlink http://www.twitch.tv/$argv[1] $argv[2]
+    streamlink --twitch-disable-ads --twitch-disable-hosting --twitch-disable-reruns --twitch-low-latency http://www.twitch.tv/$argv[1] $argv[2]
   end
 end

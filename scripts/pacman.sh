d312a13f2f6d49320c0d89ac790f6140af4dd3ed
Mark Berger <maberger6@gmail.com>
2021-01-15 20:51:17 -0500

    Add pacman script

---
+++ scripts/pacman.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+pacman -S \
+  alacritty \
+  bat \
+  code \
+  dolphin \
+  exa \
+  firefox \
+  fish \
+  galculator \
+  gimp \
+  git \
+  i3blocks \
+  inter-font \
+  keepassxc \
+  libreoffice-fresh \
+  mediainfo \
+  mpv \
+  neofetch \
+  newsboat \
+  nodejs \
+  npm \
+  rofi \
+  streamlink \
+  sublime-text \
+  syncthing \
+  unrar \
+  youtube-dl \
+  whois \
+  zathura \
+  zathura-pdf-poppler
